0322ad9404ef1c1c4c9876e334c905fd116f92a8
manojmd72 <30645416+manojmd72@users.noreply.github.com>
2017-08-06 22:25:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,6 @@
 console.log('Loaded!');
 
-/*
-var element = document.getElementById('main-text');
 
-element.innerHTML = 'New Value';
-
-var img =document.getElementById('madi');
-var marginLeft = 0;
-var temp =0;
-
-function moveRight(){
- marginLeft= marginLeft+1;
- img.style.marginLeft=marginLeft+'px';
- 
-temp =  img.style.marginLeft; 
-}
-img.onclick = function ()
-{
-    //mg.style.marginLeft='100px';i
-    var interval = setInterval(moveRight,100);    
-    };
-    
-    
-    */
 
 var button = document.getElementById('counter');
     //var counter=0;
