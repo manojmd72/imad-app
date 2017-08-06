00ae8a8187fc78f5c1a87d70216cf4a72ea1abea
manojmd72 <30645416+manojmd72@users.noreply.github.com>
2017-08-06 20:33:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,6 +26,32 @@ img.onclick = function ()
     var button = document.getElementById('counter');
     var counter=0;
     button.onclick= function(){
+        
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function()
+        {
+            if (request.readystate === XMLHttpRequest.DONE)
+            {
+                if (request.readystate===200) {
+                var counter = request.responseText;
+                var span = document.getElementById ('count');
+                span.innerHTML=counter.toString();
+                
+            }
+            
+         }
+         
+};
+         
+         request.open('GET', 'http://deshmukhmanoj.imad.hasura-app.io/counter', true);
+         request.send(null);
+};
+
+         
+         
+            
+        }
+        
         counter= counter+1;
         var span= document.getElementById('count');
         span.innerHTML= counter.toString();
