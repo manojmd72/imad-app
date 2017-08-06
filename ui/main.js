9863d879fe0543f712ceabae0dfd10ea894452c3
manojmd72 <30645416+manojmd72@users.noreply.github.com>
2017-08-06 17:31:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,13 @@ var element = document.getElementById('main.txt');
 
 var img =document.getElementById('madi');
 
+function moveright(){
+ marginLLeft= marginLeft+10;
+ img.style.marginLeft=marginLeft+'px';
+ 
+}
 img.onclick = function ()
 {
-    img.style.marginLeft='100px';
+    //mg.style.marginLeft='100px';i
+    var interval = setInterval(moveRight,100);    
 }    
