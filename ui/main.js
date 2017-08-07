3f8e481519b78cb088fd4bcf55ec2c2392173cd0
manojmd72 <30645416+manojmd72@users.noreply.github.com>
2017-08-07 22:24:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 
-var button = document.getElementById('counter');
+ var button = document.getElementById('counter');
 
 button.onclick= function(){
         var request = new XMLHttpRequest();
