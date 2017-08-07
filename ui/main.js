d51dc6c92208c079d79e45b269ed18035d19e870
manojmd72 <30645416+manojmd72@users.noreply.github.com>
2017-08-07 21:45:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ button.onclick= function(){
 };
          
          request.open('GET', 'http://deshmukhmanoj.imad.hasura-app.io/counter', true);
-         request.send(null);
+        // request.send(null);
 };
 
 
