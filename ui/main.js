b71479e81bed301f022bb67ae33fdbf4c60bdf43
manojmd72 <30645416+manojmd72@users.noreply.github.com>
2017-08-07 21:21:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,6 +14,7 @@ button.onclick= function(){
                 var span = document.getElementById('count');
                 span.innerHTML=counter.toString();
                 console.log('Success');
+                }
                 
             }
             
