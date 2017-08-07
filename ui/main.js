44215f1165db13f8438cd213653cb34acf560677
manojmd72 <30645416+manojmd72@users.noreply.github.com>
2017-08-07 20:24:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,11 +36,13 @@ button.onclick= function(){
  var name = nameInput.value;
  var submit = document.getElementById('submit_btn');
  submit.onClick = function () {
- var names = ['name1','name2','name3'];
+ 
+ var names = ['name1','name2','name3','name4'];
  var list = '';
  for (var i=0; i<names.length; i++){
   list+= '<li>'+ names[i] + '</li>';
  } 
+ 
  var ul= document.getElementById('namelist');
  ul.innerHTML= list;
  };
