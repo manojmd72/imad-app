245db08d4e746a0034b582e44e9daa8fc08efb1d
manojmd72 <30645416+manojmd72@users.noreply.github.com>
2017-08-10 19:39:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-
+/*
 
  var button = document.getElementById('counter');
 
@@ -41,6 +41,7 @@ button.onclick= function(){
  ul.innerHTML= list;
  };
  
-      
+  */
+  
             
    
