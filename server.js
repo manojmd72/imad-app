6bcc68be89a27c48f0edcc7a6441b8904ca527be
manojmd72 <30645416+manojmd72@users.noreply.github.com>
2017-08-10 19:09:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,14 +136,7 @@ var pool = new Pool(config);
 
 app.get('/test-db', function (req, res) {
     
-    pool.query('SELECT * FROM test', function(err,result)    {
-        if(err) {
-            res.status(500).send(err.toString());
-        }
-        else {
-            result.send(JSON.stringify(result));
-        }
-    });
+   
 });
 
 app.get('/', function (req, res) {
