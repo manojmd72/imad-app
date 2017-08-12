d55327021f74506fed58b1c3e968fb108cc8c4b4
manojmd72 <30645416+manojmd72@users.noreply.github.com>
2017-08-12 20:05:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,10 @@ var Pool = require('pg').Pool;
 var config = {
   user: 'deshmukhmanoj',
   database:'deshmukhmanoj',
-  host:'db.imad.hasura-app.io',
-  port:'5432',
+  host: 'db.imad.hasura-app.io',
+  port: '5432',
   password: process.env.DB_PASSWORD
 };
-
-
-alert ("hello");
 
 
 
