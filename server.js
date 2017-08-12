b3096a4ba661ac7685109917f4f91007ceff2ab1
manojmd72 <30645416+manojmd72@users.noreply.github.com>
2017-08-12 19:54:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var config = {
   user: 'deshmukhmanoj',
   database:'deshmukhmanoj',
   host:'db.imad.hasura-app.io',
-  port:'5432',
+  port:'localhost:5432',
   password: process.env.DB_PASSWORD
 };
 
