ffce1a114c315f108260b948a8181349a82369f5
manojmd72 <30645416+manojmd72@users.noreply.github.com>
2017-08-13 20:20:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ var path = require('path');
 var Pool = require('pg-pool');
 
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  
+});
+
 
 var config = {
   user: 'deshmukhmanoj',
@@ -25,10 +30,6 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-  
-});
 
 var pool = new Pool (config);
 
