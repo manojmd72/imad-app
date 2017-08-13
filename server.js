5d9c119839fad6317b88c3e7a58997d7f00e78d0
manojmd72 <30645416+manojmd72@users.noreply.github.com>
2017-08-13 20:25:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ console.log('Server Loaded!');
 
 var express = require('express');
 var morgan = require('morgan');
-
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+  user: 'deshmukhmanoj',
+  database:'deshmukhmanoj',
+  host: 'db.imad.hasura-app.io',
+  port: '5432',
+  password: 'process.env.DB_PASSWORD'
+};
+
 
 var app = express();
 app.use(morgan('combined'));
-
-//var Pool = require('pg').Pool;
-var Pool = require('pg-pool');
 
 
 app.get('/', function (req, res) {
@@ -18,29 +24,12 @@ app.get('/', function (req, res) {
 });
 
 
-var config = {
-  user: 'deshmukhmanoj',
-  database:'deshmukhmanoj',
-  host: 'db.imad.hasura-app.io',
-  port: '5432',
-  password: 'db-deshmukhmanoj-42063'
-};
-
-
-
-
-
-
-
-
 var pool = new Pool (config);
 
 app.get('/test-db', function (req, res) {
  
-   // res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-   //res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-   
     pool.query('SELECT * FROM test', function(err,result)    {
+
         if(err) {
             
             console.log('Error!');
