cbe19daec04eb9ea0ddaa3796316ed9aab93fa79
manojmd72 <30645416+manojmd72@users.noreply.github.com>
2017-08-13 21:40:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,6 +58,7 @@ app.get('/articles/:articleName', function (req, res) {
           } else {
               var articleData = result.rows[0];
               res.send(createTemplate(articleData));
+              console.log(articleData);
           }
       } 
       
