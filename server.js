f4aaf46e11e8b6655e3f19c6151033310f971186
manojmd72 <30645416+manojmd72@users.noreply.github.com>
2017-08-20 15:04:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,8 +115,7 @@ app.get('/check-login', function (req, res) {
         res.send ('You are logged in ' + req.session.auth.userId.toString());
         
     } else {
-        res.send ('You are logged in');
-        
+        res.send ('You are not logged in');
     }
 });
 
